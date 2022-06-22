6ed8c6bd392de8a15c8d36035ebacc82aebbe2b1
Art-afk <a976632@gmail.com>
2022-06-22 21:49:52 +0800

    FileWork-finish

--- part1/FileWork.java
+++ part1/FileWork.java
@@ -1,26 +1,29 @@
 package part1;
 /*
 "Во входном файле содержится случайный текст, состоящий из букв и цифр. (1 строка)\n" +
-                "Необходимо в выходной файл записать отдельно две строки - первая - все буквенные символы, вторая - все числа. (2 строка)\n" +
-                "Порядок должен сохраниться тем же, что был во входном файле (3 строка)";
+"Необходимо в выходной файл записать отдельно две строки - первая - все буквенные символы, вторая - все числа. (2 строка)\n" +
+"Порядок должен сохраниться тем же, что был во входном файле (3 строка)";
  */
 import java.io.*;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.StringJoiner;
+
 
 public class FileWork {
 
     public static void main(String[] args) throws Exception {
          String path ="/home/zar/Java/mytest.txt";
          String topatch = "/home/zar/Java/mytest2.txt";
-         String text = "Вовходномфайл1еодержитсяслуча2йныйтекстсост3оящи5йизбуквицифр4строка";
-
+         //String text = "Вовходномфайл1еодержитсяслуча2йныйтекстсост3оящи5йизбуквицифр4строка";
+        String text = "dfssdf4r43";
         WriteToFile(path, text);
         Start(path, topatch);
 
     }
     static public void WriteToFile(String path, String txt) throws Exception{
+
         try(FileOutputStream fos = new FileOutputStream(path);
             BufferedOutputStream bos = new BufferedOutputStream(fos))
         {
@@ -30,49 +33,67 @@ public class FileWork {
             System.out.println(ex.getMessage());
         }
     }
+    static public void WriteToFile(String path, String txt, String txt2) throws Exception{
+
+        try(FileOutputStream fos = new FileOutputStream(path);
+            BufferedOutputStream bos = new BufferedOutputStream(fos))
+        {   String spl = txt + txt2;
+            byte[] buff = spl.getBytes();
+            bos.write(buff, 0, buff.length);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
     static public byte[] ReadFromFile(String path) throws Exception {
         byte[] temp = new byte[0];
         try(FileInputStream fis = new FileInputStream(path);
             BufferedInputStream bfi = new BufferedInputStream(fis)){
-            byte i;
+            int i;
             int v = 0;
             byte[] mydata = new byte[bfi.available()];
-            while ((i = (byte) bfi.read()) != -1){
-                mydata[v] = i;
+            while ((i = bfi.read()) != -1){
+               mydata[v] = (byte) i;
                 v++;
+             //   System.out.println(i + "   char:   " + (char) i);
             }
             temp = mydata;
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
         return temp;
-     /*   try(FileInputStream fis = new FileInputStream(path);
-            ByteArrayOutputStream bfi = new ByteArrayOutputStream()) {
-            int val=0;
-            byte[] buffer = new byte[(int) path.length()];
-            while ((val = fis.read(buffer)) > 0)
-                bfi.write(buffer,0,1);
-            fis.close();
-            bfi.close();
-            byte[] data = bfi.toByteArray();
-
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-      */
     }
-    static public void Start (String path, String topatch) throws Exception{
-   byte[] data = ReadFromFile(path);
+    static public void Start (String path, String topatch) throws Exception {
+        byte[] data = ReadFromFile(path);
+        char[] bukvi = new char[data.length];
+        char[] ciferki = new char[data.length];
+        int i = 0;
+        int b = 0;
+        for (byte c : data) {
+            // System.out.println((char) c);
+            if (Character.isLetter((char) c)) {
+                bukvi[i] = (char) c;
+                i++;
+            } else {
+                ciferki[b] = (char) c;
+                b++;
 
-        for (byte c: data
-             ) {
-            //String out = new String(Character.toString(c));
-            System.out.println((char) c);
+            }
         }
+        char[] bukviSmall = new char[i+1];
+        bukviSmall[i] ='\n';
+        char[] ciferkiSmall = new char[b+1];
+        ciferkiSmall[b] = '\n';
+        System.arraycopy(bukvi, 0, bukviSmall, 0, i);
+        System.arraycopy(ciferki, 0, ciferkiSmall, 0, b);
+        String str = new String(bukviSmall);
+        String cif = new String(ciferkiSmall);
+
+       //System.out.println(ciferkiSmall);
+
+         WriteToFile(topatch,str,cif);
+
+
 
 
     }
-
-}
+}//class
