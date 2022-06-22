fafc801c38b369e475f7fbd0520815417a656729
Art-afk <a976632@gmail.com>
2022-06-23 01:35:45 +0800

    refactoring stroka

--- part1/QuickSort.java
+++ part1/QuickSort.java
@@ -1,50 +1,61 @@
 package part1;
-/*
-Быстроя сортировка
- */
+
 import java.util.Arrays;
 
-public class QuickSort {
+public class QuickSort  {
     public static void main(String[] args) {
-    //    int[] array = {8,5,4,6,2,1,6,5,2,10,55,13,15,16};
-        int[] array = {8,9,3,7,6,7,1};
-        quickSort(array,0,array.length - 1);
-        System.out.println(Arrays.toString(array));
+        int[] x = {9, 0, 2, 4, 5, 12, 5, 6,12 ,5 ,-43, -10, -5,11 ,14, 20, -4,-2};
+     //   int[] x = { 8, 0, 4, 7, 3, 7, 10, 12, -3 };
+        System.out.println("Было: ");
+        System.out.println(Arrays.toString(x));
+        int low = 0;
+        int high = x.length - 1;
+
+        quickSort(x, low, high);
+
+        System.out.println("Стало: ");
+        System.out.println(Arrays.toString(x));
+
     }
 
-    public static void quickSort(int[] array, int left, int right){
-    int index = 0;
-    if (array.length > 1){
-        index = partition(array,left, right);
-        if (left < index - 1){
-            quickSort(array,left, index -1);
-        }
-        if(index < right){
-            quickSort(array,index,right);
-        }
-    }
-    }
-    public static int partition(int[] array, int left, int right){
-        int centre = array[(right + left) / 2 ];
-        while (left <= right){
-            while (array[left] < centre){
-                left++;
-            }
-            while (array[right] > centre){
-                right--;
+    public static void quickSort(int[] array, int low, int high){
+        if(array.length == 0)
+            return;
 
+        if(low >= high)
+            return; //если уже нечего делить
+
+        //опорный элемент
+        int middle = low + (high - low) / 2;
+        int opora = array[middle];
+
+        //разделить на подмассивы, который больше и меньше опорного элемента
+        int i = low, j = high;
+        while (i <= j){
+            while (array[i] < opora){
+                i++;
             }
-            if (left <= right){
-                swap(array, left, right);
-                left++;
-                right--;
+            while (array[j] > opora){
+                j--;
             }
+
+
+        if (i <= j){//меняем местами
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            i++;
+            j--;
         }
-        return left;
-    }
-    private static void swap(int[] array, int firstIndex, int secondIndex){
-        int temp = array[firstIndex];
-        array[firstIndex] = array[secondIndex];
-        array[secondIndex] = temp;
-    }
-}
+        }
+// вызов рекурсии для сортировки левой и правой части
+        if (low <j)
+            quickSort(array,low,j);
+
+        if(high>i)
+            quickSort(array,i, high);
+
+
+    }// quickSort
+
+} //class
--- part1/Stroka.java
+++ part1/Stroka.java
@@ -1,17 +1,30 @@
 package part1;
 /*
 Реализуйте алгоритм, определяющий, все ли символы в строке встречаются только один раз.
+
+
+уменьшить в половину длинну поиска
  */
 
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Stroka {
     public static void main(String[] args) {
         Scanner myScannerIn = new Scanner(System.in);
         System.out.println("Введите строку которую надо проверить: ");
-        String str = myScannerIn.nextLine();
+        long m = System.currentTimeMillis();
+      //  String str = myScannerIn.nextLine();
+        String str = "asdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadhogertyhjzxvnkalrjhytgOGIUUDHFGUAldgh5et3405680hrgkldzfnblN'o;IHRGoiehr4803IOHSGHGhisasdilhgiadho";
+
         int i = 0;
+        int low =0;
+        int hight = str.length()-1;
+        int medi = (low + hight) / 6;
+        System.out.println("Средне число " + medi);
         int count = 0;
+        byte[] stoka = str.getBytes(StandardCharsets.UTF_8);
+        /*
         while ( i < str.length()-1) {
         int len = str.length()-1;
             while (len > i) {
@@ -22,10 +35,23 @@ public class Stroka {
             }
            i++;
         }
-        if (count > 0) System.out.println("В строке ЕСТЬ повторяющиеся символы");
-        else System.out.println("В строке НЕТ повторяющихся символов");
 
 
+
+
+      //  for ()
+
+        //   if (count > 0) System.out.println("В строке ЕСТЬ повторяющиеся символы");
+        //else System.out.println("В строке НЕТ повторяющихся символов");
+
+
+         */
+        for (byte c: stoka
+             ) {
+            System.out.println("byte: " + c + " Char to: " + (char) c);
+        }
+
+        System.out.println((double) (System.currentTimeMillis() - m));
 
 
     } //main
