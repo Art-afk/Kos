828b28d613a189a45158fd7060431951e8195247
Art-afk <a976632@gmail.com>
2022-06-23 13:51:11 +0800

    hz

---
+++ part1/StrokaTwo.java
@@ -0,0 +1,31 @@
+package part1;
+
+import java.util.Scanner;
+
+public class StrokaTwo {
+    public static void main(String[] args) {
+        Scanner myScannerIn = new Scanner(System.in);
+      //  System.out.println("Введите строку которую надо проверить: ");
+      //  String str = myScannerIn.nextLine();
+            String str = "/метод, в котором выводится средняя оценка";
+
+
+
+        char [] chars = str.toCharArray();
+        //System.out.println(chars[1]);
+        for (char c: chars) {
+            System.out.println(c);
+
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+}
