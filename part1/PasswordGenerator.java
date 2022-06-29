557103e570bd0a3d1724baa6aede8570527a094a
Art-afk <a976632@gmail.com>
2022-06-30 00:08:03 +0800

    add "PasswordGenerator"

---
+++ part1/PasswordGenerator.java
@@ -0,0 +1,104 @@
+package part1;
+/*
+Написать алгоритм генерации пароля на основании входных данных - длина последовательности, наличие заглавных букв, цифр, спецсимволов.
+Для упрощения можно написать метод, который на входе получает необходимые параметры.
+ */
+
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.*;
+import java.util.stream.IntStream;
+
+
+public class PasswordGenerator {
+    public static void main(String[] args) {
+        boolean upper = false;
+        boolean number = false;
+        boolean scharacters = false;
+        Scanner src = new Scanner(System.in);
+        System.out.println("Какой длинны нужен пароль?");
+        int longs = Integer.parseInt(src.nextLine());
+        System.out.println("Сколько сгенерировать?");
+        int longsT = Integer.parseInt(src.nextLine());
+        System.out.println("Использовать Цифры?(yes/NO");
+        if (src.nextLine().equalsIgnoreCase("yes"))
+            number = true;
+        System.out.println("Использовать Заглавные буквы?(yes/NO");
+        if (src.nextLine().equalsIgnoreCase("yes"))
+            upper = true;
+        System.out.println("Использовать спецсимволы?(yes/NO");
+        if (src.nextLine().equalsIgnoreCase("yes"))
+            scharacters = true;
+
+        for (int i = 0; i <= longsT; i++) {
+            String password = "";
+            for (int ac = 0; ac <= longs - 1; ac++) {
+                password = password + (char) Swap(number, upper, scharacters);
+            }
+            System.out.println(password);
+        }
+    }
+
+    public static int RandomAlphaBig() {
+        Random r = new Random();
+        char c = (char) (r.nextInt(26) + 'A'); //all BIG alphabet
+        return c;
+    }
+
+    public static int RandomAlphaSmall() {
+        Random r = new Random();
+        char c = (char) (r.nextInt(26) + 'a'); //all BIG alphabet
+        return c;
+    }
+
+    public static int RandomAllNumber() {
+        Random r = new Random();
+        char c = (char) (r.nextInt(10) + '0'); //all numbers
+        return c;
+    }
+
+    public static int RandomAllSymbol() {
+        Random r = new Random();
+        char a = (char) (r.nextInt(16) + ' '); //first scpecsimbol alphabe
+        char b = (char) (r.nextInt(7) + ':'); //first scpecsimbol alphabe
+        char c = (char) (r.nextInt(6) + '['); //first scpecsimbol alphabe
+        char d = (char) (r.nextInt(4) + '{'); //first scpecsimbol alphabe
+        int[] arr = {a, b, c, d};
+        int i = r.nextInt(3);
+        return arr[i];
+    }
+
+    public static int Swap(boolean number, boolean upper, boolean schar) {
+        int num = -1;
+        int up = -1;
+        int sch = -1;
+        if (number) {
+            num = RandomAllNumber();
+
+        }
+        if (upper) {
+            int b = 0;
+            int c = 0;
+            b = RandomAlphaBig();
+            c = RandomAlphaSmall();
+            int[] arr = {b, c};
+            Random r1 = new Random();
+            int i = r1.nextInt(1);
+            up = arr[i];
+        }
+        if (schar)
+            sch = RandomAllSymbol();
+
+        Random r2 = new Random();
+        int[] arr = {num, up, sch};
+        while (true) {
+            int i = r2.nextInt(2);
+            if (arr[i] >= 0) {
+                return arr[i];
+            }
+        }
+
+    }
+
+
+}
