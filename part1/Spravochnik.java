ebe8ddd18de67fbddc98917669c7c160d31b2409
Art-afk <a976632@gmail.com>
2022-08-04 23:31:54 +0800

    finish first point

--- part1/Spravochnik.java
+++ part1/Spravochnik.java
@@ -1,12 +1,11 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
+import java.util.Collections;
 
 /*
 Необходимо создать справочник с типом. <Имя, Зарплата>, добавить туда 10 имён, зарплаты внести
 случайным образом от 25 000 до 100 000, зарплата должна быть кратна 1000. Имена задаются разработчиком.
 Затем:
-1.Вывести имя человека с самой высокой и самой низкой зарплатой
+Вывести имя человека с самой высокой и самой низкой зарплатой
 2.Вывести имена  всех, у кого зарплата выше средней
 3.Посчитать зарплату после уплаты налогов и вывести в формате <Имя> - <на руки> - <налог>
  */
@@ -15,30 +14,41 @@ public class Spravochnik {
     public static void main(String[] args) {
         int minSalary = 25000;
         int maxSalary = 100000;
-        createPeople(minSalary,maxSalary);
-
-
-
+        workPeopleBase(minSalary, maxSalary);
+    }
+    public static void workPeopleBase(int minSalary, int maxSalary) {
+        Map<String, Integer> peopleSalary = new HashMap<String, Integer>();
+        peopleSalary.put("Vasya", randomSalaryGet(minSalary, maxSalary));//1
+        peopleSalary.put("Petya", randomSalaryGet(minSalary, maxSalary));//2
+        peopleSalary.put("Olga", randomSalaryGet(minSalary, maxSalary));//3
+        peopleSalary.put("Anna", randomSalaryGet(minSalary, maxSalary));//4
+        peopleSalary.put("Kostya", randomSalaryGet(minSalary, maxSalary));//5
+        peopleSalary.put("Korge", randomSalaryGet(minSalary, maxSalary));//6
+        peopleSalary.put("Kolya", randomSalaryGet(minSalary, maxSalary));//7
+        peopleSalary.put("Taniya", randomSalaryGet(minSalary, maxSalary));//8
+        peopleSalary.put("Vasilisa", randomSalaryGet(minSalary, maxSalary));//9
+        peopleSalary.put("Marina", randomSalaryGet(minSalary, maxSalary));//10
+        System.out.println(peopleSalary); //debug
+        ArrayList<Integer> values = new ArrayList<>(peopleSalary.values());
+        int minimumSalary = checkMinMaxSalary(values, "min");
+        int maximumISalary = checkMinMaxSalary(values, "max");
+        //System.out.println(minimumSalary); //debug
+        System.out.println("Minimum salary: " + getKeyFromValue(peopleSalary, minimumSalary) + " "+ minimumSalary);
+        System.out.println("Maximum salary: " + getKeyFromValue(peopleSalary, maximumISalary) + " "+ maximumISalary);
+       checkAndReturnAboveAverageMediumSalary(peopleSalary,values);
 
     }
-    public static void createPeople(int minSalary,int maxSalary){
-        Map<String,Integer> peopleSalary = new HashMap<String,Integer>();
-        peopleSalary.put("Vasya", randomSalaryGet(minSalary,maxSalary));//1
-        peopleSalary.put("Petya", randomSalaryGet(minSalary,maxSalary));//2
-        peopleSalary.put("Olga", randomSalaryGet(minSalary,maxSalary));//3
-        peopleSalary.put("Anna", randomSalaryGet(minSalary,maxSalary));//4
-        peopleSalary.put("Kostya", randomSalaryGet(minSalary,maxSalary));//5
-        peopleSalary.put("Korge", randomSalaryGet(minSalary,maxSalary));//6
-        peopleSalary.put("Kolya", randomSalaryGet(minSalary,maxSalary));//7
-        peopleSalary.put("Taniya", randomSalaryGet(minSalary,maxSalary));//8
-        peopleSalary.put("Vasilisa", randomSalaryGet(minSalary,maxSalary));//9
-        peopleSalary.put("Marina", randomSalaryGet(minSalary,maxSalary));//10
-        //{Petya=96000, Kostya=74000, Taniya=50000, Olga=88000, Kolya=92000, Vasilisa=51000, Vasya=31000, Korge=49000, Anna=59000, Marina=61000}
-
-
+    public static Object getKeyFromValue(Map hm, Object value){
+        for (Object o: hm.keySet()
+             ) {
+            if (hm.get(o).equals(value)){
+                return o;
+            }
+        }
+        return null;
     }
 
-    public static int randomSalaryGet(int min, int max){
+    public static int randomSalaryGet(int min, int max) {
         while (true) {
             Random r = new Random();
             int diff = max - min;
@@ -48,6 +58,31 @@ public class Spravochnik {
                 return i;
             }
         }
+    }//randomSalaryGet
+
+    public static int checkMinMaxSalary(ArrayList list, String minMax) {
+      //  System.out.println(list);
+        int i = 0;
+        if (minMax == "min" || minMax == "") {
+            int minimum = (int) Collections.min(list);
+            return minimum;
+        }
+        else { //minMax = max
+            int maximum = (int) Collections.max(list);
+            return maximum;
+        }
+    } //checkMinMaxSalary
+
+    public static void checkAndReturnAboveAverageMediumSalary(Map peopleMap,ArrayList salaryArray){
+        int summ = 0;
+        for (Object c: salaryArray
+             ) {
+            System.out.println(c);
+           summ = (int) c + summ;
+        }
+        System.out.println(summ / 2);
+
+
 
     }
 
