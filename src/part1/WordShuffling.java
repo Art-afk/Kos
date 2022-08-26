b3bec6f4577da4fc2031646cb0a897d0f7a4fb48
Art-afk <a976632@gmail.com>
2022-08-26 16:08:50 +0800

    refactoring

--- src/part1/WordShuffling.java
+++ src/part1/WordShuffling.java
@@ -1,17 +1,24 @@
+/**
+ * @author zar
+ */
 package part1;
-/*
-Во входном файле набор строк, состоящий из нескольких слов, слова разделены пробелами, знаки препинания и спец символы отсутствуют. Регистр нижний.
-В выходной файл необходимо вывести текст "задом наперёд" - поменять местами строки и слова внутри строк, например:
-Входные данные:
-Жужжали мухи
-Светил фонарик
-кипела вода в чайнике
 
-Выходные данные:
-чайнике в вода кипела
-фонарик светил
-мухи жужжали
-
+/**
+ * Во входном файле набор строк, состоящий из нескольких слов, слова разделены пробелами, знаки препинания и спец символы отсутствуют. Регистр нижний.
+ * В выходной файл необходимо вывести текст "задом наперёд" - поменять местами строки и слова внутри строк, например:
+ * Входные данные:
+ * Жужжали мухи
+ * Светил фонарик
+ * кипела вода в чайнике
+ * <p>
+ * Выходные данные:
+ * чайнике в вода кипела
+ * фонарик светил
+ * мухи жужжали
+ *
+ *
+ * Алгоритм:
+ * загоняем всё в ArrayList и просто выводим всё с конца
  */
 
 import java.io.BufferedInputStream;
@@ -34,13 +41,16 @@ public class WordShuffling {
         // Start(path, topatch);
         ReadFromFile(path);
 
-
-
     }
 
-
-    public static void WriteToFile(String path,String text) throws Exception {
-        try(FileOutputStream fos = new FileOutputStream(path,false);
+    /**
+     *
+     * @param path to the file to write to
+     * @param text to write to file
+     * @throws Exception something wrong
+     */
+    public static void WriteToFile(String path, String text) throws Exception {
+        try (FileOutputStream fos = new FileOutputStream(path, false);
         ) {
             fos.write(text.getBytes(StandardCharsets.UTF_16));
             fos.close();
@@ -48,18 +58,23 @@ public class WordShuffling {
         }
     }
 
-    public static char ReadFromFile(String path) throws Exception{
-        try(FileInputStream fis = new FileInputStream(path);
-            BufferedInputStream bio = new BufferedInputStream(fis);
+    /**
+     *
+     * @param path to the file to be read
+     * @return return data from file
+     * @throws Exception something wrong
+     */
+    public static char ReadFromFile(String path) throws Exception {
+        try (FileInputStream fis = new FileInputStream(path);
+             BufferedInputStream bio = new BufferedInputStream(fis);
         ) {
             int i;
 
-            while ((i= bio.read()) != -1){
-             //   System.out.println((char)i);
+            while ((i = bio.read()) != -1) {
+                //   System.out.println((char)i);
             }
-            return(char) i;
+            return (char) i;
         }
-
 
 
     }
