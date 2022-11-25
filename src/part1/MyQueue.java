2c02918f87665f23daad3332346ea1debb7b8f7e
zar <aka.zarj@gmail.com>
2022-11-25 13:18:54 +0600

    refactoring

--- src/part1/MyQueue.java
+++ src/part1/MyQueue.java
@@ -6,31 +6,55 @@ public class MyQueue {
 
     private Stack<Object> stackMain = new Stack<Object>();
 
-    public void addFirst(Object object) {
+    public void add(Object object) {
         stackMain.add(object);
     }
 
-    public void addLast(Object object) {
-        Stack<Object> stackTakeover = new Stack<Object>();
-        for (Object st : stackMain) {
-            stackTakeover.add(st);
-        }
-        stackTakeover.add(object);
+    public void clear() {
         stackMain.clear();
-        for (Object st : stackTakeover) {
-            stackMain.add(st);
-        }
-
     }
 
     public Object element() {
         if (stackMain.empty()) throw new NoSuchElementException();
-        else return stackMain.firstElement();
+        else {
+            Stack<Object> stackTakeover = new Stack<Object>();
+            Object ret;
+            if (stackMain.empty()) throw new NoSuchElementException();
+            else {
+                Iterator<Object> itr = stackMain.iterator();
+                while (itr.hasNext()) {
+                    stackTakeover.push(stackMain.pop());
+                }
+                ret = stackTakeover.peek();
+                Iterator<Object> itr2 = stackTakeover.iterator();
+                while (itr2.hasNext()) {
+                    stackMain.push(stackTakeover.pop());
+                }
+            }
+            return ret;
+
+        }
     }
 
     public Object peek() {
-        if (stackMain.empty()) throw new NoSuchElementException();
-        else return stackMain.firstElement();
+        if (stackMain.empty()) return null;
+        else {
+            Stack<Object> stackTakeover = new Stack<Object>();
+            Object ret;
+            if (stackMain.empty()) throw new NoSuchElementException();
+            else {
+                Iterator<Object> itr = stackMain.iterator();
+                while (itr.hasNext()) {
+                    stackTakeover.push(stackMain.pop());
+                }
+                ret = stackTakeover.peek();
+                Iterator<Object> itr2 = stackTakeover.iterator();
+                while (itr2.hasNext()) {
+                    stackMain.push(stackTakeover.pop());
+                }
+            }
+            return ret;
+        }
     }
 
     /**
@@ -78,18 +102,7 @@ public class MyQueue {
         }
     }
 
-    public Object getLast() {
-        return stackMain.peek();
-
-    }
-
-    public Object getFirst() {
-
-        return stackMain.firstElement();
-
-    }
-
-    public Object prt() {
+    public boolean print() {
         for (Object item : stackMain) {
             System.out.println(item);
         }
--- src/part1/MyQueueApp.java
+++ src/part1/MyQueueApp.java
@@ -8,34 +8,21 @@ public class MyQueueApp {
 
     public static void main(String[] args) {
         MyQueue myQueue = new MyQueue();
-        myQueue.addFirst("1");
-        myQueue.addFirst("2");
-        myQueue.addFirst("3");
-        myQueue.addFirst("4");
-        myQueue.addFirst("5");
-        System.out.println(myQueue.prt());
-        System.out.println("First: " + myQueue.getFirst());
-        System.out.println("Last: " + myQueue.getLast());
-        System.out.println("Element: " + myQueue.element());
-        System.out.println("Remove: " + myQueue.remove());
-        System.out.println("Pool: " + myQueue.pool());
-        System.out.println(myQueue.prt());
-        qua(); //debug for me
-
+        myQueue.add("1");
+        myQueue.add("2");
+        myQueue.add("3");
+        myQueue.add("4");
+        myQueue.add("5");
+        System.out.println("Element: " + myQueue.element()); //возвращает элемент из головы очереди. Элемент не удаляется. Если очередь пуста, инициируется исключение NoSuchElementException.
+        System.out.println("Remove: " + myQueue.remove()); // удаляет элемент из головы очереди, возвращая его. Инициирует исключение NoSuchElementException, если очередь пуста.
+        System.out.println(myQueue.print());
+        System.out.println("Pool: " + myQueue.pool());//возвращает элемент из головы очереди и удаляет его. Возвращает null, если очередь пуста.
+        System.out.println("Peek: " + myQueue.peek());//возвращает элемент из головы очереди. Возвращает null, если очередь пуста. Элемент не удаляется.
+        System.out.println(myQueue.print());
+        myQueue.clear();
 
     }
 
-    public static void qua(){
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(1);
-        queue.add(2);
-        queue.add(3);
-        queue.add(4);
-        queue.add(5);
-        System.out.println(queue);
-        queue.poll();
-        System.out.println(queue);
-        }
-    }
+}
 
 
