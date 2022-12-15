9a1cb874eaea9ec409bf202b956a7ac3e2e84c85
Art-afk <94371440+Art-afk@users.noreply.github.com>
2022-12-15 14:01:55 +0800

    refactoring

--- src/part1/MyQueue.java
+++ src/part1/MyQueue.java
@@ -14,63 +14,58 @@ public class MyQueue {
         stackMain.clear();
         stackTakeover.clear();
     }
+
     public Object element() {
         if (stackTakeover.empty() && !stackMain.empty()) {
-            Iterator<Object> itr = stackMain.iterator();
-            while (itr.hasNext()) {
-                stackTakeover.push(stackMain.pop());
-            }
+            revertStack();
         } else if (stackTakeover.empty() && stackMain.empty()) {
             throw new NoSuchElementException();
         }
         return stackTakeover.peek();
     }
+
     public Object peek() {
         if (stackTakeover.empty() && !stackMain.empty()) {
-            Iterator<Object> itr = stackMain.iterator();
-            while (itr.hasNext()) {
-                stackTakeover.push(stackMain.pop());
-            }
+            revertStack();
         } else if (stackTakeover.empty() && stackMain.empty()) {
             return null;
         }
         return stackTakeover.peek();
     }
+
     public Object remove() {
         if (stackTakeover.empty() && !stackMain.empty()) {
-            Iterator<Object> itr = stackMain.iterator();
-            while (itr.hasNext()) {
-                stackTakeover.push(stackMain.pop());
-            }
+            revertStack();
         } else if (stackTakeover.empty() && stackMain.empty()) {
             throw new NoSuchElementException();
-        }
-        return stackTakeover.pop();
-    }
-    public Object pool() {
-        if (stackTakeover.empty() && !stackMain.empty()) {
-            Iterator<Object> itr = stackMain.iterator();
-            while (itr.hasNext()) {
-                stackTakeover.push(stackMain.pop());
-            }
-        } else if (stackTakeover.empty() && stackMain.empty()) {
-            return null;
         }
         return stackTakeover.pop();
     }
 
+    public Object pool() {
+        if (stackTakeover.empty() && !stackMain.empty()) {
+            revertStack();
+        } else if (stackTakeover.empty() && stackMain.empty()) {
+            return null;
+        }
+        return stackTakeover.pop();
+    }
 
 
     public Object print() {
         if (stackTakeover.empty() && !stackMain.empty()) {
-            Iterator<Object> itr = stackMain.iterator();
-            while (itr.hasNext()) {
-                stackTakeover.push(stackMain.pop());
-            }
+            revertStack();
         } else if (stackTakeover.empty() && stackMain.empty()) {
             return null;
         }
         return stackTakeover.toString();
 
+    }
+
+    private void revertStack() {
+        Iterator<Object> itr = stackMain.iterator();
+        while (itr.hasNext()) {
+            stackTakeover.push(stackMain.pop());
+        }
     }
 }
